a1f15242110a283ee9038e615069dcc5d65618af
Neyzoouuww <hgmarlier13@gmail.com>
2021-04-22 09:17:17 +0200

    update2

---
+++ src/main/java/galaria/item/AmuletteSmeltItem.java
@@ -0,0 +1,48 @@
+
+package galaria.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import galaria.itemgroup.GalariaItemGroup;
+
+import galaria.GalariaModElements;
+
+@GalariaModElements.ModElement.Tag
+public class AmuletteSmeltItem extends GalariaModElements.ModElement {
+	@ObjectHolder("galaria:amulette_smelt")
+	public static final Item block = null;
+	public AmuletteSmeltItem(GalariaModElements instance) {
+		super(instance, 56);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(GalariaItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("amulette_smelt");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
---
+++ src/main/java/galaria/item/HammerSmeltGalariaItem.java
@@ -0,0 +1,52 @@
+
+package galaria.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import galaria.itemgroup.GalariaItemGroup;
+
+import galaria.GalariaModElements;
+
+@GalariaModElements.ModElement.Tag
+public class HammerSmeltGalariaItem extends GalariaModElements.ModElement {
+	@ObjectHolder("galaria:hammer_smelt_galaria")
+	public static final Item block = null;
+	public HammerSmeltGalariaItem(GalariaModElements instance) {
+		super(instance, 57);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 5000;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -3f, new Item.Properties().group(GalariaItemGroup.tab)) {
+		}.setRegistryName("hammer_smelt_galaria"));
+	}
+}
